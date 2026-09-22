e75909eaaa345005be8ab4b8acb47e9179669c88
Lokesh <vadlamudiloki90@gmail.com>
2026-09-22 16:37:07 +0000

    feat:Add Comments for better understanding in TseitinEncoding code

--- tseiten/tseitin.cpp
+++ tseiten/tseitin.cpp
@@ -1,6 +1,13 @@
 #pragma once
 #include "../cnf/cnf.h"
 
+//This function basically gives us the req Tseiten encoded CNF formula
+//  with its PropLookup and IdLookup containing the aux vars, their ids.
+//It works on the principle of recursion first we separate the root,
+//  so that we can recurse we get that cnf from the next func.
+//Here we invoke some functions at the start which basically assign aux ids to every internal node in the tree
+//  and also build up our final cnf's lookup maps.
+//At last we insert the root unit clause and return.  
 CNF TseitinEncodedCNF(Formula f){
     CNF tseitin = CNF();
     Node* root = f.root;
@@ -18,6 +25,9 @@ CNF TseitinEncodedCNF(Formula f){
     return clauses;
 }
 
+//This is the function which recurses without the root in its clauses.
+//It basically checks over the operator at the root and adds the required clause and recurses deeper into the tree.
+//At last merged with the smaller sub CNF formulas.
 CNF TseitinWithoutRootRecursive(Node* root){
     CNF tseitin = CNF();
     if(!root) return tseitin;
@@ -66,6 +76,9 @@ CNF TseitinWithoutRootRecursive(Node* root){
     return tseitin;
 }
 
+//This function is called in the beginning of TseitinEncodedCNF.
+//It basically takes the root,Proplookup and adds the new aux variables in the map 
+//  corresponding them to their new ids(aux_ids)(which we generate in AssignAuxId function).
 void AuxPropLookup(Node* root,unordered_map<std::string,int> &lookup){
     if(!root) return;
     //if leaf also we dont need to make a new aux var
@@ -76,7 +89,9 @@ void AuxPropLookup(Node* root,unordered_map<std::string,int> &lookup){
     lookup[auxvar] = root->aux_id; 
     return;
 }
-
+//This function is called in the beginning of TseitinEncodedCNF.
+//It basically takes the root,Idlookup and adds the new aux_id in the map 
+//  corresponding them to their aux_vars(which we generate in AssignAuxId function).
 void AuxIdLookup(Node* root,unordered_map<int,std::string> &Idlookup){
     if(!root) return;
     if(!root->left && !root->right) return;
@@ -85,7 +100,7 @@ void AuxIdLookup(Node* root,unordered_map<int,std::string> &Idlookup){
     Idlookup[root->aux_id]="a_"+std::to_string(root->aux_id);
     return;
 }
-
+//assigns aux_ids(increasing from bottom to top) to internal nodes and for atomic props assigns aux_id as the id thats already given for it in lookup
 void AssignAuxId(Node* root,int& nextid,unordered_map<std::string,int>& lookup){
     if(!root) return;
     if(!root->left&&!root->right) {root->aux_id = lookup[root->nodeString];return;}
